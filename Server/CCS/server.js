52d848d26419e58fe63f1e53618bc94c25afd7f9
Seshpenguin <seshan10@me.com>
2016-07-10 23:11:52 -0400

    stuff

---
+++ Server/CCS/server.js
@@ -0,0 +1,6 @@
+//CCS (Central Communication Server) System 1.0
+//(c) 2016 EstiNet
+//For use only on an authorized EstiIoT Server. Unauthorized use is prohibited.
+
+//Node.JS Modules
+var io = require('socket.io')();
